dea26bcd943700aeaea0c56a00c4f2e1f04cf3bf
Thomas Koehler <basta.t.k+git@gmail.com>
2014-06-21 11:15:43 +0200

    Make b2WeldJoint C Interface

---
+++ src/c_box2d/dynamics/joints/weld_joint.cpp
@@ -0,0 +1,41 @@
+b2WeldJointDef WeldJointDef_default() {
+    return b2WeldJointDef();
+}
+void WeldJointDef_initialize(b2WeldJointDef* self,
+                                 b2Body* body_a, b2Body* body_b,
+                                 const b2Vec2* anchor) {
+    self->Initialize(body_a, body_b, *anchor);
+}
+
+b2Joint* WeldJoint_as_joint(b2WeldJoint* self) {
+    return static_cast<b2Joint*>(self);
+}
+b2WeldJoint* Joint_as_weld_joint(b2Joint* self) {
+    return static_cast<b2WeldJoint*>(self);
+}
+
+const b2Vec2* WeldJoint_get_local_anchor_a(const b2WeldJoint* self) {
+    return &self->GetLocalAnchorA();
+}
+const b2Vec2* WeldJoint_get_local_anchor_b(const b2WeldJoint* self) {
+    return &self->GetLocalAnchorB();
+}
+f32 WeldJoint_get_reference_angle(const b2WeldJoint* self) {
+    return self->GetReferenceAngle();
+}
+void WeldJoint_set_frequency(b2WeldJoint* self, f32 frequency) {
+    self->SetFrequency(frequency);
+}
+f32 WeldJoint_get_frequency(const b2WeldJoint* self) {
+    return self->GetFrequency();
+}
+void WeldJoint_set_damping_ratio(b2WeldJoint* self, f32 ratio) {
+    self->SetDampingRatio(ratio);
+}
+f32 WeldJoint_get_damping_ratio(const b2WeldJoint* self) {
+    return self->GetDampingRatio();
+}
+
+void WeldJoint_dump(b2WeldJoint* self) {
+    self->Dump();
+}

87a318eff9c17e098705b2f0eccc9c380b3aa274
Thomas Koehler <basta.t.k+git@gmail.com>
2014-06-21 11:15:29 +0200

    Make b2RopeJoint C Interface

--- src/c_box2d/c_box2d.cpp
+++ src/c_box2d/c_box2d.cpp
@@ -28,3 +28,5 @@ typedef double f64;
 #include "c_box2d/dynamics/joints/prismatic_joint.cpp"
 #include "c_box2d/dynamics/joints/pulley_joint.cpp"
 #include "c_box2d/dynamics/joints/revolute_joint.cpp"
+#include "c_box2d/dynamics/joints/rope_joint.cpp"
+#include "c_box2d/dynamics/joints/weld_joint.cpp"
---
+++ src/c_box2d/dynamics/joints/rope_joint.cpp
@@ -0,0 +1,30 @@
+b2RopeJointDef RopeJointDef_default() {
+    return b2RopeJointDef();
+}
+
+b2Joint* RopeJoint_as_joint(b2RopeJoint* self) {
+    return static_cast<b2Joint*>(self);
+}
+b2RopeJoint* Joint_as_rope_joint(b2Joint* self) {
+    return static_cast<b2RopeJoint*>(self);
+}
+
+const b2Vec2* RopeJoint_get_local_anchor_a(const b2RopeJoint* self) {
+    return &self->GetLocalAnchorA();
+}
+const b2Vec2* RopeJoint_get_local_anchor_b(const b2RopeJoint* self) {
+    return &self->GetLocalAnchorB();
+}
+void RopeJoint_set_max_length(b2RopeJoint* self, f32 length) {
+    self->SetMaxLength(length);
+}
+f32 RopeJoint_get_max_length(const b2RopeJoint* self) {
+    return self->GetMaxLength();
+}
+b2LimitState RopeJoint_get_limit_state(const b2RopeJoint* self) {
+    return self->GetLimitState();
+}
+
+void RopeJoint_dump(b2RopeJoint* self) {
+    self->Dump();
+}

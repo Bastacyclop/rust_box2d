39781c1beeeb36339a431eec658892a3e946eb86
Thomas Koehler <basta.t.k+git@gmail.com>
2014-06-21 10:51:59 +0200

    Make b2PulleyJoint C Interface

--- src/c_box2d/c_box2d.cpp
+++ src/c_box2d/c_box2d.cpp
@@ -26,3 +26,4 @@ typedef double f64;
 #include "c_box2d/dynamics/joints/motor_joint.cpp"
 #include "c_box2d/dynamics/joints/mouse_joint.cpp"
 #include "c_box2d/dynamics/joints/prismatic_joint.cpp"
+#include "c_box2d/dynamics/joints/pulley_joint.cpp"
---
+++ src/c_box2d/dynamics/joints/pulley_joint.cpp
@@ -0,0 +1,48 @@
+b2PulleyJointDef PulleyJointDef_default() {
+    return b2PulleyJointDef();
+}
+void PulleyJointDef_initialize(b2PulleyJointDef* self,
+                               b2Body* body_a, b2Body* body_b,
+                               const b2Vec2* ground_anchor_a,
+                               const b2Vec2* ground_anchor_b,
+                               const b2Vec2* anchor_a,
+                               const b2Vec2* anchor_b,
+                               f32 ratio) {
+    self->Initialize(body_a, body_b,
+                     *ground_anchor_a, *ground_anchor_b,
+                     *anchor_a, *anchor_b,
+                     ratio);
+}
+
+b2Joint* PulleyJoint_as_joint(b2PulleyJoint* self) {
+    return static_cast<b2Joint*>(self);
+}
+b2PulleyJoint* Joint_as_pulley_joint(b2Joint* self) {
+    return static_cast<b2PulleyJoint*>(self);
+}
+
+b2Vec2 PulleyJoint_get_ground_anchor_a(const b2PulleyJoint* self) {
+    return self->GetGroundAnchorA();
+}
+b2Vec2 PulleyJoint_get_ground_anchor_b(const b2PulleyJoint* self) {
+    return self->GetGroundAnchorB();
+}
+f32 PulleyJoint_get_length_a(const b2PulleyJoint* self) {
+    return self->GetLengthA();
+}
+f32 PulleyJoint_get_length_b(const b2PulleyJoint* self) {
+    return self->GetLengthB();
+}
+f32 PulleyJoint_get_ratio(const b2PulleyJoint* self) {
+    return self->GetRatio();
+}
+f32 PulleyJoint_get_current_length_a(const b2PulleyJoint* self) {
+    return self->GetCurrentLengthA();
+}
+f32 PulleyJoint_get_current_length_b(const b2PulleyJoint* self) {
+    return self->GetCurrentLengthB();
+}
+
+void PulleyJoint_dump(b2PulleyJoint* self) {
+    self->Dump();
+}

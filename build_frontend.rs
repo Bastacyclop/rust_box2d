56e2358a09fe88f21e3838666386a45c72aaf2e6
Thomas Koehler <basta.t.k+git@gmail.com>
2015-07-16 18:39:07 +0200

    Add optional environment variable to find box2d include files

--- build_frontend.rs
+++ build_frontend.rs
@@ -3,10 +3,16 @@ use std::process::Command;
 use std::path::Path;
 
 fn main() {
+    let box2d_include =
+        match env::var("BOX2D_INCLUDE_PATH") {
+            Ok(path) => String::from("-I") + &path,
+            Err(_) => String::new()
+        };
     let out_dir = env::var("OUT_DIR").unwrap();
 
     Command::new("g++").args(&["-c", "src/box2d_frontend.cpp", "-o",
                              &format!("{}/box2d_frontend.o", out_dir),
+                             &box2d_include,
                              "-std=c++11", "-Isrc", "-fPIC", "-Wall"])
                        .status().unwrap();
     Command::new("ar").args(&["crus", "libbox2d_frontend.a", "box2d_frontend.o"])

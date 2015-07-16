0193754846e1316fb280b9a600fcc58d28596347
Thomas Koehler <basta.t.k+git@gmail.com>
2015-07-16 19:07:18 +0200

    Use gcc crate for build script

--- build_frontend.rs
+++ build_frontend.rs
@@ -1,23 +1,28 @@
+extern crate gcc;
+
 use std::env;
 use std::process::Command;
 use std::path::Path;
 
 fn main() {
-    let box2d_include =
-        match env::var("BOX2D_INCLUDE_PATH") {
-            Ok(path) => String::from("-I") + &path,
-            Err(_) => String::new()
-        };
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    Command::new("g++").args(&["-c", "src/box2d_frontend.cpp", "-o",
-                             &format!("{}/box2d_frontend.o", out_dir),
-                             &box2d_include,
-                             "-std=c++11", "-Isrc", "-fPIC", "-Wall"])
-                       .status().unwrap();
-    Command::new("ar").args(&["crus", "libbox2d_frontend.a", "box2d_frontend.o"])
-                      .current_dir(&Path::new(&out_dir))
-                      .status().unwrap();
+    let mut config = gcc::Config::new();
+    let config = config
+        .cpp(true)
+        .flag("-c")
+        .flag("-std=c++11")
+        //.flag("-fPIC")
+        .flag("-Wall")
+        .file("src/box2d_frontend.cpp")        
+        .include("src");
+        
+    let config = match env::var("BOX2D_INCLUDE_PATH") {
+        Ok(path) => config.include(path),
+        Err(_) => config
+    };
+    
+    config.compile("libbox2d_frontend.a");
 
     println!("cargo:rustc-link-search=native={}", out_dir);
     println!("cargo:rustc-link-lib=static=box2d_frontend");

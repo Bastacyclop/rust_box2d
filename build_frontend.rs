9b35168a39a39968f45960425ac91fcd32590d8c
Thomas Koehler <basta.t.k+git@gmail.com>
2015-07-19 06:15:23 +0200

    Fix travis

--- build_frontend.rs
+++ build_frontend.rs
@@ -6,10 +6,10 @@ fn main() {
     let mut config = gcc::Config::new();
     let config = config
         .cpp(true)
-        .flag("-Wall")
         .file("frontend/lib.cpp")
         .include("src");
 
+    config.compile("libbox2d_frontend.a");
     let config = match env::var("BOX2D_INCLUDE_PATH") {
         Ok(path) => config.include(path),
         Err(_) => config

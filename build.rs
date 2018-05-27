47f4f84bd8ce6c33ccb1d540dc8f18a6a6a3bb54
Gabriel Konat <gabrielkonat@gmail.com>
2018-05-27 10:45:25 +0200

    Build and link Box2D in the build script.
    Build and link the Box2D submodule in build.rs, not requiring users to have Box2D installed on their system.
    This significantly reduces the effort to use this library on Windows, since it does not have a package manager to install Box2D.
    This does require users to have CMake installed, because this is what Box2D uses as its build system.

--- build.rs
+++ build.rs
@@ -1,19 +1,20 @@
-extern crate gcc;
-
-use std::env;
+extern crate cc;
+extern crate cmake;
 
 fn main() {
-    let mut config = gcc::Config::new();
-    let config = config
+    let box2d_install_prefix = cmake::Config::new("Box2D/Box2D")
+        .define("BOX2D_BUILD_STATIC", "ON")
+        .define("BOX2D_INSTALL", "ON")
+        .define("BOX2D_BUILD_SHARED", "OFF")
+        .define("BOX2D_BUILD_EXAMPLES", "OFF")
+        .define("BOX2D_INSTALL_DOC", "OFF")
+        .build();
+    println!("cargo:rustc-link-search=native={}/lib", box2d_install_prefix.display());
+    println!("cargo:rustc-link-lib=static=Box2D");
+
+    cc::Build::new()
         .cpp(true)
-        .file("frontend/lib.cpp");
-
-    let config = match env::var("BOX2D_INCLUDE_PATH") {
-        Ok(path) => config.include(path),
-        Err(_) => config
-    };
-
-    config.compile("libbox2d_frontend.a");
-
-    println!("cargo:rustc-flags=-l Box2D");
+        .include("Box2D/Box2D")
+        .file("frontend/lib.cpp")
+        .compile("libbox2d_frontend.a");
 }
